6c3eb0fab0c2cf9526e8586dec85297ba6ec092d
ahmadsaufi211001011 <ahmadsaufi2110010119@gmail.com>
2023-05-16 21:50:39 +0630

    program SatgasCovidApp 1.3

---
+++ 2110010119 Ahmad Saufi/src/satgascoviddesaapp/AduanCovid.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package satgascoviddesaapp;
+
+/**
+ *
+ * @author pc
+ */
+public class AduanCovid {
+    public Integer id_laporan;
+    public String nomor_aduan;
+    public String judul;
+    public String isi;
+    public String file;
+    public String status;
+    public Integer user;
+    public String create_data;
+    public String update_date;
+}
